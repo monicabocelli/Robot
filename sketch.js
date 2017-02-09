ccf328f75bd575af18ad235d38570ab94aa09644
monicabocelli <monica.bocelli93@gmail.com>
2017-02-09 18:18:45 +0100

    Uploaded files

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(720,480);
+  background(230);
+  colorMode(RGB);
+}
+
+function draw() {
+  strokeWeight(2);
+  //capelli
+  line(340,130,325,105);
+  line(370,125,380,60);
+  line(350,195,350,270);
+  //collo
+  line(360,195,360,270);
+  line(370,195,370,270);
+  line(395,150,430,130);
+  //testa
+  fill(0);
+  ellipse(360,160,70,70);
+  fill(140);
+  noStroke();
+  ellipse(350,170,5,5);
+  ellipse(350,140,4,4);
+  ellipse(375,180,3,3);
+  fill(250);
+  ellipse(370,155,30,30);
+  fill(100);
+  ellipse(370,155,5,5);
+  //corpo
+  fill(200);
+  ellipse(350,420,55,55);
+  fill(0);
+  rect(310,270,80,150);
+  fill(200);
+  rect(310,280,80,8);
+}
